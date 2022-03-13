a4138093074501bf4a6e2c3f8a2262f59a580049
Will Stuckey <guyfleeman@gmail.com>
2022-03-13 16:02:57 -0400

    embedded deps added

--- flake.nix
+++ flake.nix
@@ -1,11 +1,23 @@
 {
   description = "The firmware repository for the SSL A-Team.";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:  
+      let pkgs = nixpkgs.legacyPackages.${system};
+
+      packageName = "ateam-firmware";
+
+      in {
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ cmake gcc-arm-embedded-10 openocd ];
+        };
+      }
+    );
 }
