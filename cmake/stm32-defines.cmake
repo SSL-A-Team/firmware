2885880c4e4eefa30edd7c1f0e299891d44b7914
Will Stuckey <guyfleeman@gmail.com>
2022-03-27 17:00:09 -0400

    initial flat bin build

--- cmake/stm32-defines.cmake
+++ cmake/stm32-defines.cmake
@@ -2,9 +2,9 @@
 #  Nucleo F429ZI  #
 ###################
 
-set(NUCLEO_F429ZI_MACHINE_OPTIONS "-mthumb -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard" CACHE STRING "")
-set(NUCLEO_F429ZI_C_OPTIONS "${NUCLEO_F429ZI_MACHINE_OPTIONS}")
-set(NUCLEO_F429ZI_CXX_OPTIONS "${NUCLEO_F429_MACHINE_OPTIONS} -Wno-register -fno-exceptions -fno-rtti" CACHE STRING "")
-set(NUCLEO_F429ZI_LINKER_OPTIONS "--specs=nosys.specs --specs=nano.specs -u _printf_float" CACHE STRING "")
-set(NUCLEO_F429ZI_DEFINITIONS "-DSTM32F429XX")
+set(NUCLEO_F429ZI_MACHINE_OPTIONS -mthumb -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard CACHE STRING "")
+set(NUCLEO_F429ZI_C_OPTIONS ${NUCLEO_F429ZI_MACHINE_OPTIONS})
+set(NUCLEO_F429ZI_CXX_OPTIONS ${NUCLEO_F429ZI_MACHINE_OPTIONS} -Wno-register -fno-exceptions -fno-rtti CACHE STRING "")
+set(NUCLEO_F429ZI_LINKER_OPTIONS ${NUCLEO_F429ZI_MACHINE_OPTIONS} --specs=nosys.specs --specs=nano.specs -u _printf_float CACHE STRING "")
+set(NUCLEO_F429ZI_DEFINITIONS -DSTM32F429xx)
 set(NUCLEO_F429ZI_CHIP STM32F429ZI CACHE STRING "Full STM32 Chip Model")
