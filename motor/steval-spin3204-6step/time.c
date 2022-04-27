62ec42fe9dae22fc6174168a896cb17feabf5773
Will Stuckey <guyfleeman@gmail.com>
2022-04-27 18:50:46 -0400

    synch function

--- motor/steval-spin3204-6step/time.c
+++ motor/steval-spin3204-6step/time.c
@@ -11,8 +11,14 @@
 
 #include <stm32f031x6.h>
 
+#include <stdbool.h>
+
 #include "time.h"
 
+/**
+ * @brief blocks for a number of milliseconds
+ * 
+ */
 __attribute__((optimize("O0")))
 void wait_ms(uint32_t time_ms) {
     for (uint32_t count = 0; count < time_ms; count++) {
@@ -20,4 +26,20 @@ void wait_ms(uint32_t time_ms) {
         // the SysTick timer will continue running and we assume it was set to begin with
         while ((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) == 0);
     }
+}
+
+/**
+ * @brief blocks until SysTick fires
+ * 
+ * @return true if the tick has already elapsed and no waiting occurred
+ *          can be used to determine if a core process is not meeting timing slack requirements
+ */
+__attribute__((optimize("O0")))
+bool sync_ms() {
+    if ((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) != 0) {
+        return true;
+    }
+
+    while ((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) == 0);
+    return false;
 }
