d7ee1f15744285c59eb980727e2b4964d3aaf632
Will Stuckey <guyfleeman@gmail.com>
2022-08-21 22:47:57 -0400

    uart dma rx working

--- motor/src/bin/radio-rt/main.rs
+++ motor/src/bin/radio-rt/main.rs
@@ -39,8 +39,8 @@ use stm32h7xx_hal::dma::{
 //
 // The runtime does not initialise these SRAM banks
 const TX_BUFFER_LEN: usize = 16;
-const RX_BUFFER_LEN: usize = 16;
-const RX_BUFFER_DEPTH: usize = 2;
+const RX_BUFFER_LEN: usize = 32;
+const RX_BUFFER_DEPTH: usize = 5;
 
 
 
@@ -78,10 +78,20 @@ static mut radio_dma_rx_transfer: Option<RadioDmaRxTrs> = None;
 static mut radio_dma_rx_config: Option<DmaConfig> = None;
 // TODO probs make a simple queue struct so we can control the placement of backing memory in axisram
 // use existing if able
+static mut radio_dma_rx_count: usize = 0;
 static mut radio_dma_rx_read_ptr: usize = 0;
 static mut radio_dma_rx_write_ptr: usize = 0;
+static mut radio_uart_rx_ring_buf_of_err: bool = false;
 #[link_section = ".axisram.buffers"]
-static mut radio_uart_rx_buf: [[u8; RX_BUFFER_LEN]; RX_BUFFER_DEPTH] = [[0; RX_BUFFER_LEN]; RX_BUFFER_DEPTH];
+static mut radio_uart_rx_buf: [([u8; RX_BUFFER_LEN], usize); RX_BUFFER_DEPTH] = [([0; RX_BUFFER_LEN], 0); RX_BUFFER_DEPTH];
+
+/**
+ * @param number of segment available to read
+ * 
+ * @return number of segments read
+ */
+type FnDmaRxCallback = fn(usize) -> usize;
+static mut radio_uart_rx_callback: Option<FnDmaRxCallback> = None;
 
 #[entry]
 fn main() -> ! {
@@ -137,6 +147,11 @@ fn main() -> ! {
         )
         .unwrap();
 
+    serial.unlisten(Event::Rxne);
+    serial.unlisten(Event::Rxftie);
+    serial.listen(Event::Idle);
+    serial.clear_idle();
+
     let (radio_uart_tx_ch, radio_uart_rx_ch) = serial.split();
 
     //////////////////////////
@@ -167,7 +182,7 @@ fn main() -> ! {
     }
 
     let dma_rx_config = DmaConfig::default()
-            .memory_increment(false)
+            .memory_increment(true)
             .transfer_complete_interrupt(true)
             .transfer_error_interrupt(true);
     unsafe { radio_dma_rx_config = Some(dma_rx_config); }
@@ -176,48 +191,92 @@ fn main() -> ! {
     radio_dma_rx_stream.set_transfer_complete_interrupt_enable(true);
 
     unsafe {
-    let transfer: RadioDmaRxTrs = 
+    let mut transfer: RadioDmaRxTrs = 
         Transfer::init(
             radio_dma_rx_stream,
             radio_uart_rx_ch,
-            &mut radio_uart_rx_buf[0],
+            &mut (radio_uart_rx_buf[radio_dma_rx_write_ptr].0),
             None,
             radio_dma_rx_config.as_ref().unwrap().clone());
+
+    transfer.start(|serial| (
+        serial.enable_dma_rx()
+    ));
 
     radio_dma_rx_transfer = Some(transfer);
     }
 
+
+
     unsafe {
+        (*pac::USART2::ptr()).cr1.modify(|_, w| w.rxneie().clear_bit());
+        (*pac::USART2::ptr()).rqr.write(|w| w.rxfrq().set_bit());
+        //(*pac::USART2::ptr()).icr.write(|w| w.rx.set_bit());
         pac::NVIC::unmask(pac::Interrupt::DMA1_STR0);
+        //pac::NVIC::unmask(pac::Interrupt::DMA1_STR1);
         pac::NVIC::unmask(pac::Interrupt::USART2);
     }
+
+    //enable_dma_rx();
 
     let mut delay = cp.SYST.delay(ccdr.clocks);
     let mut p = gpiod.pd7.into_push_pull_output();
     p.set_low();
     delay.delay_us(10u16);
 
+
+
     unsafe {
+
+    uart_dma_transfer(&[0xAAu8, 0x00, 0xAA, 00, 0xAA, 0x00, 0xAA, 00, 0xAA, 0x00, 0xAA, 00, 0xAA, 0x00, 0xAA, 00]);
+
     loop {
-        p.set_high();
-        uart_dma_transfer(&[0xFFu8, 0x00, 0xFF, 0x00]);
-        p.set_low();
+        //uart_dma_transfer(&[0xFFu8, 0x00, 0xFF, 0x00]);
+        // wait_for_transmission();
+        // delay.delay_us(10u16);
+        // uart_dma_transfer(&[0xAAu8, 0x00, 0xAA, 00, 0xAA, 0x00, 0xAA, 00, 0xAA, 0x00, 0xAA, 00, 0xAA, 0x00, 0xAA, 00]);
+        //wait_for_transmission();
+
+        //p.set_low();
         //led_r.unwrap().set_high();
-        wait_for_transmission();
+        //wait_for_transmission();
+    
+        //delay.delay_us(500u16);
+        //delay.delay_ms(500u16);
+
+        while rx_is_empty() {}
+        if rx_has_segment() {
+            let mut buf = [0; RX_BUFFER_LEN];
+            let dat = rx_read_segment(&mut buf).unwrap();
+
+            uart_dma_transfer(dat);
+
+            // p.set_high();
+            // delay.delay_us(500u16);
+            // p.set_low();
+            // delay.delay_us(500u16);
+            //let mut dest = [0; 32];
+            //let _buf = rx_read_segment(&mut dest).expect("valid buf");
+            // uart_dma_transfer(_buf);
+            // wait_for_transmission();
+            //hprintln!("got a segment!\r\n")
+        }
 
         //hprintln!("2");
 
-        delay.delay_us(500u16);
+        //delay.delay_us(500u16);
+        //delay.delay_ms(500u16);
 
-        uart_dma_transfer(&[0xAAu8, 0x00, 0xAA, 00]);
-        wait_for_transmission();
 
-        delay.delay_us(500u16);
+        // uart_dma_transfer(&[0xAAu8, 0x00, 0xAA, 00]);
+        // wait_for_transmission();
 
-        uart_dma_transfer(&[0xAAu8, 0x00, 0xAA, 00, 0xAA, 0x00, 0xAA, 00, 0xAA, 0x00, 0xAA, 00, 0xAA, 0x00, 0xAA, 00]);
-        wait_for_transmission();
+        // delay.delay_us(500u16);
 
-        delay.delay_us(500u16);
+        // uart_dma_transfer(&[0xAAu8, 0x00, 0xAA, 00, 0xAA, 0x00, 0xAA, 00, 0xAA, 0x00, 0xAA, 00, 0xAA, 0x00, 0xAA, 00]);
+        // wait_for_transmission();
+
+        // delay.delay_us(500u16);
 
     }
     }
@@ -330,6 +389,102 @@ fn USART2() {
 
             // flag transfer complete
             radio_dma_transfer_pending = false;
+        } else if isr.read().idle().bit_is_set() {
+            // cause: line idle, rx received a frame
+
+            // return;
+
+            // this global transfer holds the stream and peripherial references
+            // it should only be taken and then restored by this function
+            // if it's None, something's gone horribly wrong
+            if radio_dma_rx_transfer.is_none() {
+                hprintln!("dma rx transfer state was none when state is strictly managed internal to this function");
+                hprintln!("\twas initialization invalid or was the internal state management changed?");
+                //return Err(UartDmaError::InternalStateErr)
+
+                icr.write(|w| w.idlecf().set_bit());
+
+                if let Some(lg) = led_g.as_mut() {
+                    lg.set_low();
+                }
+
+                return;
+            }
+
+
+
+            //loop {}
+
+            // func(current_buffer, internal_cur_buf, remaining_bytes)
+            let _result = radio_dma_rx_transfer.as_mut().unwrap().next_transfer_with(|_, _, remaining_bytes| {
+                let rx_size = RX_BUFFER_LEN - remaining_bytes;
+                radio_uart_rx_buf[radio_dma_rx_write_ptr].1 = rx_size;
+
+                // if let Some(ly) = led_y.as_mut() {
+                //     if rx_size == 16 && radio_uart_rx_buf[radio_dma_rx_write_ptr].0[0] == 0xAAu8 && radio_uart_rx_buf[radio_dma_rx_write_ptr].0[1] == 0x00u8 {
+                //         ly.set_high();
+                //     }
+                // }
+
+                // check if ring buffer is full
+                if (radio_dma_rx_write_ptr + 1) % radio_uart_rx_buf.len() == radio_dma_rx_read_ptr {
+                    if let Some(lr) = led_r.as_mut() {
+                        lr.set_high();
+                    }
+
+                    radio_uart_rx_ring_buf_of_err = true;
+                } else {
+                    if let Some(lg) = led_g.as_mut() {
+                        lg.set_high();
+                    }
+
+                    radio_dma_rx_write_ptr = (radio_dma_rx_write_ptr + 1) % radio_uart_rx_buf.len();
+                    radio_dma_rx_count += 1;
+                }
+
+                (&mut (radio_uart_rx_buf[radio_dma_rx_write_ptr].0), 0)
+            });
+
+            // TODO callback? 
+
+            // // take the old transfer, reclaim ownership of hardware in preparation for the next transfer
+            // let dma_transfer = radio_dma_rx_transfer.take().unwrap();
+            // let (stream, serial, _, opt_buf) = dma_transfer.free();
+
+            // // increment the write ptr
+            // radio_dma_rx_write_ptr += 1;
+
+            // // create the next rx transfer
+            // let dma_transfer: RadioDmaRxTrs = 
+            //     Transfer::init(
+            //         stream, 
+            //         serial, 
+            //         &mut radio_uart_rx_buf[radio_dma_rx_write_ptr], 
+            //         opt_buf, 
+            //         radio_dma_rx_config.as_ref().unwrap().clone());
+
+            // // restore the global state
+            // radio_dma_rx_transfer = Some(dma_transfer);
+
+            // // start the transfer by setting the flag and enabling DMA
+            // radio_dma_rx_transfer.as_mut().unwrap().start(|serial| {
+            //     serial.enable_dma_rx();
+            // });
+
+            icr.write(|w| w.idlecf().set_bit());
+
+            if let Some(lr) = led_r.as_mut() {
+                lr.set_low();
+            }
+
+            if let Some(ly) = led_y.as_mut() {
+                ly.set_low();
+            }
+
+            if let Some(lg) = led_g.as_mut() {
+                lg.set_low();
+            }
+
         }
 
         // TODO hand other errors
@@ -376,15 +531,85 @@ fn DMA1_STR0() {
 }
 
 fn enable_dma_rx() {
-
+    unsafe {
+        pac::NVIC::unmask(pac::Interrupt::DMA1_STR1);
+    }
 }
 
 fn disable_dma_rx() {
-
+    unsafe {
+        pac::NVIC::mask(pac::Interrupt::DMA1_STR1);
+    }
 }
 
-fn set_dma_rx_callback() {
+fn set_dma_rx_callback(func: FnDmaRxCallback) {
+    unsafe {
+        radio_uart_rx_callback = Some(func);
+    }
+}
 
+fn clear_dma_rx_callback() {
+    unsafe {
+        radio_uart_rx_callback = None;
+    }
+}
+
+fn rx_has_segment() -> bool {
+    rx_get_num_readable_segments() > 0
+}
+
+fn rx_is_empty() -> bool {
+    !rx_has_segment()
+}
+
+fn rx_get_num_readable_segments() -> usize {
+    unsafe {
+        return radio_dma_rx_count
+    }
+}
+
+fn rx_read_segment(dest: &mut [u8]) -> Result<& mut [u8], ()> {
+    if rx_is_empty() {
+        //return Err(());
+    }
+
+    // len check
+
+    unsafe {
+        let buf = &radio_uart_rx_buf[radio_dma_rx_read_ptr].0;
+        let len = radio_uart_rx_buf[radio_dma_rx_read_ptr].1;
+        dest[..len].copy_from_slice(&buf[..len]);
+
+        radio_dma_rx_read_ptr = (radio_dma_rx_read_ptr + 1) % radio_uart_rx_buf.len();
+        radio_dma_rx_count -= 1;
+
+        return Ok(&mut dest[..len]);
+    }
+}
+
+fn rx_peek_segment() -> Result<&'static [u8], ()> {
+    if rx_is_empty() {
+        return Err(());
+    }
+
+    unsafe {
+        let buf = &radio_uart_rx_buf[radio_dma_rx_read_ptr].0;
+        let len = radio_uart_rx_buf[radio_dma_rx_read_ptr].1;
+        return Ok(&buf[..len]);
+    }
+}
+
+fn rx_drop_segment() -> Result<usize, ()> {
+    if rx_is_empty() {
+        return Err(())
+    }
+
+    unsafe {
+        radio_dma_rx_read_ptr = (radio_dma_rx_read_ptr + 1) % radio_uart_rx_buf.len();
+        radio_dma_rx_count -= 1;
+    }
+
+    return Ok(rx_get_num_readable_segments());
 }
 
 /**
@@ -394,7 +619,12 @@ fn set_dma_rx_callback() {
 fn DMA1_STR1() {
     unsafe {
         if let Some(rx) = radio_dma_rx_transfer.as_mut() {
-            
+            // accidentially rx the entire buffer (no idle timeout)
+            // check for an error here, tx get error flag not currently exposed
+
+            if rx.get_transfer_complete_flag() {
+                rx.clear_transfer_complete_interrupt();
+            }
         }
     }
 }
