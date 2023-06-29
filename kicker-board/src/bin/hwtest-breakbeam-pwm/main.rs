795f4ef028b272b10ce08ad5f08699be710c5429
Joe Spall <jspall0327@gmail.com>
2023-06-28 20:00:35 -0700

    WIP pwm reading work

--- kicker-board/src/bin/hwtest-breakbeam-pwm/main.rs
+++ kicker-board/src/bin/hwtest-breakbeam-pwm/main.rs
@@ -2,103 +2,105 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use core::sync::atomic::AtomicU32;
+
 use cortex_m::prelude::_embedded_hal_blocking_delay_DelayUs;
 use defmt::*;
 use {defmt_rtt as _, panic_probe as _};
 use embassy_executor::Spawner;
 use embassy_stm32::{
-    executor::InterruptExecutor,
     exti::ExtiInput,
     gpio::{Input, Level, Output, Pull, Speed},
     interrupt::{self, InterruptExt},
     time::mhz,
     usart::{self, Uart},
-    pwm::{Channel, Channel3Pin}, peripherals::PA2
+    pwm::{Channel, Channel3Pin}
 };
 use embassy_time::{Duration, Ticker, Timer};
 use ateam_kicker_board::{
-    drivers::{breakbeam_pwm::BreakbeamPwm}
+   drivers::{breakbeam_pwm::BreakbeamPwm}
 };
 use ateam_kicker_board::pins::{BlueStatusLedPin, GreenStatusLedPin, BreakbeamTxPin, BreakbeamRxPin};
 use embassy_stm32::pwm::simple_pwm::{PwmPin, SimplePwm};
 use embassy_stm32::time::khz;
+use static_cell::StaticCell;
+use embassy_executor::InterruptExecutor;
+
+static EXECUTOR_BREAKBEAM_PWM_RX: StaticCell<InterruptExecutor<interrupt::typelevel::I2C1>> = StaticCell::new();
+static BREAKBEAM_RX_COUNT: AtomicU32 = AtomicU32::new(0);
+
+#[embassy_executor::task]
+async fn poll_counter() {
+    loop {
+        let shared_var = BREAKBEAM_RX_COUNT.load(Ordering::Relaxed);
+        BREAKBEAM_RX_COUNT.store(shared_var.wrapping_add(1), Ordering::Relaxed);
+    }
+}
 
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
+    info!("breakbeam startup!");
+    
+    let mut status_led_green = Output::new(p.PA11, Level::Low, Speed::Medium);
+    let mut status_led_blue = Output::new(p.PA8, Level::Low, Speed::Medium);
 
-    let Ch3 = PwmPin::new_ch3(p.PA2);
-    let mut pwm = SimplePwm::new(p.TIM2, None, None, Some(Ch3), None, khz(10));
-    let max = pwm.get_max_duty();
-    pwm.enable(Channel::Ch3);
 
-    info!("PWM initialized");
-    info!("PWM max duty {}", max);
+    // Breakbeam 
+    let mut breakbeam = BreakbeamPwm::<_,_,_>::new_ch3(
+        p.PB0,
+        Channel::Ch3, 
+        p.TIM3,
+        10,
+        p.PA3);
 
-    loop {
-        pwm.set_duty(Channel::Ch3, 0);
-        Timer::after(Duration::from_millis(300)).await;
-        pwm.set_duty(Channel::Ch3, max / 4);
-        Timer::after(Duration::from_millis(300)).await;
-        pwm.set_duty(Channel::Ch3, max / 2);
-        Timer::after(Duration::from_millis(300)).await;
-        pwm.set_duty(Channel::Ch3, max - 1);
-        Timer::after(Duration::from_millis(300)).await;
+    let breakbeam_irq = interrupt::take!(I2C1);
+    breakbeam_irq.set_priority(interrupt::typelevel::Priority::P6);
+    let executor = EXECUTOR_BREAKBEAM_PWM_RX.init(InterruptExecutor::new(breakbeam_irq));
+    let spawner = executor.start();
+    info!("start");
+
+    spawner.spawn(poll_counter()).unwrap();
+
+    status_led_green.set_high();
+    Timer::after(Duration::from_millis(250)).await;
+    status_led_green.set_low();
+    Timer::after(Duration::from_millis(250)).await;
+    status_led_green.set_high();
+    Timer::after(Duration::from_millis(250)).await;
+    status_led_green.set_low();
+    Timer::after(Duration::from_millis(250)).await;
+
+    loop 
+    {
+        // Enable transmitter, wait until it is ready, drive blue status LED if receiving, wait 1 sec
+        breakbeam.enable_tx();
+        let count = BREAKBEAM_RX_COUNT.load(Ordering::Relaxed);
+        BREAKBEAM_RX_COUNT.store(0, Ordering::Relaxed);
+
+        info!("count:{}", count);
+        if breakbeam.read()
+        {
+            status_led_blue.set_high();
+        } 
+        else
+        {
+            status_led_blue.set_low();
+        }
+        Timer::after(Duration::from_millis(100)).await;
+
+        // Disable transmitter, wait until it is ready, drive blue status LED if receiving, wait 1 sec
+        breakbeam.disable_tx();
+        if breakbeam.read()
+        {
+            status_led_blue.set_high();
+        } 
+        else
+        {
+            status_led_blue.set_low();
+        }
+        Timer::after(Duration::from_millis(100)).await;
+        
     }
 }
-//async fn main(_spawner: Spawner) {
-//    let p = embassy_stm32::init(Default::default());
-//    info!("breakbeam startup!");
-//    
-//    let mut status_led_green = Output::new(p.PA11, Level::Low, Speed::Medium);
-//    let mut status_led_blue = Output::new(p.PA8, Level::Low, Speed::Medium);
-//
-//    // Breakbeam 
-//    let breakbeam_int = interrupt::take!(SPI1);
-//    let mut breakbeam = BreakbeamPwm::<_,_,_,_>::new_ch3(
-//        p.PA2,
-//        Channel::Ch3, 
-//        p.TIM2,
-//        10,
-//        p.PA3,
-//        breakbeam_int);
-//
-//    status_led_green.set_high();
-//    Timer::after(Duration::from_millis(250)).await;
-//    status_led_green.set_low();
-//    Timer::after(Duration::from_millis(250)).await;
-//    status_led_green.set_high();
-//    Timer::after(Duration::from_millis(250)).await;
-//    status_led_green.set_low();
-//    Timer::after(Duration::from_millis(250)).await;
-//
-//    loop 
-//    {
-//        // Enable transmitter, wait until it is ready, drive blue status LED if receiving, wait 1 sec
-//        breakbeam.enable_tx();
-//        if breakbeam.read()
-//        {
-//            status_led_blue.set_high();
-//        } 
-//        else
-//        {
-//            status_led_blue.set_low();
-//        }
-//        Timer::after(Duration::from_millis(1000)).await;
-//
-//        // Disable transmitter, wait until it is ready, drive blue status LED if receiving, wait 1 sec
-//        //breakbeam.disable_tx();
-//        //if breakbeam.read()
-//        //{
-//        //    status_led_blue.set_high();
-//        //} 
-//        //else
-//        //{
-//        //    status_led_blue.set_low();
-//        //}
-//        //Timer::after(Duration::from_millis(1000)).await;
-//        
-//    }
-//}
--- kicker-board/src/drivers/breakbeam_pwm.rs
+++ kicker-board/src/drivers/breakbeam_pwm.rs
@@ -1,4 +1,6 @@
 use core::marker::PhantomData;
+use core::sync::atomic::AtomicU32;
+
 use embassy_stm32::gpio::{Input, Pin, Pull};
 use embassy_stm32::pwm::simple_pwm::{PwmPin, SimplePwm};
 use embassy_stm32::pwm::{Channel as PwmChannel, CaptureCompare16bitInstance, Channel1Pin, Channel2Pin, Channel3Pin, Channel4Pin};
@@ -6,35 +8,33 @@ use embassy_stm32::time::khz;
 use embassy_stm32::Peripheral;
 use embassy_time::{Duration, Timer};
 use embassy_stm32::interrupt::Interrupt;
+use embassy_executor::{raw::TaskStorage, SpawnToken};
 
 
 pub struct BreakbeamPwm<'a,
     PinTx: Pin, 
     PwmTx: CaptureCompare16bitInstance, 
-    PinRx: Pin,
-    IrqRx: Interrupt> 
+    PinRx: Pin> 
 {
-    phantom: PhantomData<(PinTx, PinRx, IrqRx)>,
+    phantom: PhantomData<(PinTx, PinRx)>,
     pwm_tx: SimplePwm<'a, PwmTx>,
     pwm_ch_tx: PwmChannel,
-    pwm_max_duty: u16,
+    pwm_max_duty: u16
 }
 
 
 impl<'a,
     PwmTx: CaptureCompare16bitInstance,
     PinTx: Pin + Channel1Pin<PwmTx>,
-    PinRx: Pin,
-    IrqRx: Interrupt>
-    BreakbeamPwm<'a, PinTx, PwmTx, PinRx, IrqRx> 
+    PinRx: Pin>
+    BreakbeamPwm<'a, PinTx, PwmTx, PinRx> 
 {
     pub fn new_ch1(            
         pin_tx: PinTx, 
         pwm_ch_tx: PwmChannel, 
         timer_tx:  impl Peripheral<P = PwmTx> + 'a, 
         freq_tx: u32, 
-        pin_rx: PinRx,
-        int_ch_rx: impl Peripheral<P = PinRx::ExtiChannel> + 'a
+        pin_rx: PinRx
     ) -> Self 
     {
         let pwm_ch_tx = pwm_ch_tx;
@@ -62,17 +62,15 @@ impl<'a,
 impl<'a,
     PwmTx: CaptureCompare16bitInstance,
     PinTx: Pin + Channel2Pin<PwmTx>,
-    PinRx: Pin,
-    IrqRx: Interrupt>
-    BreakbeamPwm<'a, PinTx, PwmTx, PinRx, IrqRx> 
+    PinRx: Pin>
+    BreakbeamPwm<'a, PinTx, PwmTx, PinRx> 
 {
     pub fn new_ch2(            
         pin_tx: PinTx, 
         pwm_ch_tx: PwmChannel, 
         timer_tx:  impl Peripheral<P = PwmTx> + 'a, 
         freq_tx: u32, 
-        pin_rx: PinRx,
-        int_ch_rx: impl Peripheral<P = PinRx::ExtiChannel> + 'a
+        pin_rx: PinRx
     ) -> Self 
     {
         let pwm_ch_tx = pwm_ch_tx;
@@ -102,17 +100,15 @@ impl<'a,
 impl<'a,
     PwmTx: CaptureCompare16bitInstance,
     PinTx: Pin + Channel3Pin<PwmTx>,
-    PinRx: Pin,
-    IrqRx: Interrupt>
-    BreakbeamPwm<'a, PinTx, PwmTx, PinRx, IrqRx> 
+    PinRx: Pin>
+    BreakbeamPwm<'a, PinTx, PwmTx, PinRx> 
 {
     pub fn new_ch3(            
         pin_tx: PinTx, 
         pwm_ch_tx: PwmChannel, 
         timer_tx:  impl Peripheral<P = PwmTx> + 'a, 
         freq_tx: u32, 
-        pin_rx: PinRx,
-        irq: IrqRx
+        pin_rx: PinRx
     ) -> Self 
     {
         let pwm_ch_tx = pwm_ch_tx;
@@ -145,44 +141,36 @@ impl<'a,
         
     }
 
+    #[inline]
     pub fn enable_tx(&mut self)
     {
         self.pwm_tx.set_duty(self.pwm_ch_tx, self.pwm_max_duty/2);
-        //Timer::after(Duration::from_millis(400)).await;
-    }
-
-    pub fn disable_tx(&mut self)
-    {
-        self.pwm_tx.set_duty(self.pwm_ch_tx, 0);
-        //Timer::after(Duration::from_millis(400)).await;
     }
 
     #[inline]
+    pub fn disable_tx(&mut self)
+    {
+        self.pwm_tx.set_duty(self.pwm_ch_tx, 0);
+    }
+
     pub fn read(&self) -> bool
     {
         true
-    }
-
-    fn on_interrupt(_: *mut ()) 
-    {
-        
     }
 }
 
 impl<'a,
     PwmTx: CaptureCompare16bitInstance,
     PinTx: Pin + Channel4Pin<PwmTx>,
-    PinRx: Pin,
-    IrqRx: Interrupt>
-    BreakbeamPwm<'a, PinTx, PwmTx, PinRx, IrqRx> 
+    PinRx: Pin>
+    BreakbeamPwm<'a, PinTx, PwmTx, PinRx> 
 {
     pub fn new_ch4(            
         pin_tx: PinTx, 
         pwm_ch_tx: PwmChannel, 
         timer_tx:  impl Peripheral<P = PwmTx> + 'a, 
         freq_tx: u32, 
-        pin_rx: PinRx,
-        int_ch_rx: impl Peripheral<P = PinRx::ExtiChannel> + 'a
+        pin_rx: PinRx
     ) -> Self 
     {
         let pwm_ch_tx = pwm_ch_tx;
