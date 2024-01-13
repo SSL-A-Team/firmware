4b4272ba06212e81d478e193762dce75d185aed4
Will Stuckey <guyfleeman@gmail.com>
2024-01-13 13:27:06 -0500

    IMU WIP

--- control-board/src/drivers/imu/bmi085.rs
+++ control-board/src/drivers/imu/bmi085.rs
@@ -11,6 +11,37 @@ pub struct Bmi085<'a, T: spi::Instance, TxDmaCh, RxDmaCh, AccelCsPin: Pin, GyroC
     gyro_cs: Output<'a, GyroCsPin>,
 }
 
+enum AccelRegisters {
+    ACC_CHIP_ID = 0x00,
+    ACC_ERR_REG = 0x02,
+    ACC_STATUS = 0x03,
+    ACC_X_LSB = 0x12,
+    ACC_X_MSB = 0x13,
+    ACC_Y_LSB = 0x14,
+    ACC_Y_MSB = 0x15,
+    ACC_Z_LSB = 0x16,
+    ACC_Z_MSB = 0x17,
+    SENSORTIME_0 = 0x18,
+    SENSORTIME_1 = 0x19,
+    SENSORTIME_2 = 0x1A,
+    ACC_INT_STAT_1 = 0x1D,
+    TEMP_MSB = 0x22,
+    TEMP_LSB = 0x23,
+    ACC_CONF = 0x40,
+    ACC_RANGE = 0x41,
+    INT1_IO_CTRL = 0x53,
+    INT2_IO_CTRL = 0x54,
+    INT_MAP_DATA = 0x58,
+    ACC_SELF_TEST = 0x6D,
+    ACC_PWR_CONF = 0x7C,
+    ACC_PWR_CTRL = 0x7D,
+    ACC_SOFTRESET = 0x7E,
+}
+
+enum GyroRegisters {
+
+}
+
 impl<'a, T: spi::Instance, TxDmaCh, RxDmaCh, AccelCsPin: Pin, GyroCsPin: Pin> Bmi085<'a, T, TxDmaCh, RxDmaCh, AccelCsPin, GyroCsPin> {
     pub fn new_from_spi(
         spi: spi::Spi<'a, T, TxDmaCh, RxDmaCh>, 
@@ -55,6 +86,8 @@ impl<'a, T: spi::Instance, TxDmaCh, RxDmaCh, AccelCsPin: Pin, GyroCsPin: Pin> Bm
         }
     }
 
-    
+    fn self_test(&self) {
+        
+    }
 
 }
