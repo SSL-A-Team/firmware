0ea7e243d6ca4293028ee37a7968210a9eaf2aaf
Will Stuckey <guyfleeman@gmail.com>
2024-05-05 16:10:15 -0400

    add some error checking to RA shim to warn in invalid environments

--- util/rust-analyzer-shim.bash
+++ util/rust-analyzer-shim.bash
@@ -6,4 +6,17 @@
 # and the hashed path changes, or a user wants to use a specific version on
 # the path. This shell script shims the analyzer using the which command.
 
+if ! command -v rust-analyzer; then
+    echo "Rust Analyzer Shim Script Error. There is no rust-analyzer on the path."
+    echo "Did you start VS code from the Nix environemnt? OR Is the rust cross compiler installed?"
+    exit 1
+fi
+
+if ! which rust-analyzer | grep -q "nix"; then
+    echo "Rust Analyzer Shim Script Error. rust-analyzer is on path but is not in the Nix store."
+    echo "This script is intended to support the Nix environment on Linux, Mac, and WSL2"
+    echo "If you are not using the Nix env, please modify the project .vscode/settings.json to set your own rust language server"
+    exit 1
+fi
+
 exec $(which rust-analyzer) "$@"
