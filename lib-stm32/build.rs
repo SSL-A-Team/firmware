1847d197a50d7a9e6d0ad15866879aa4f9099a11
Christian Clark <christianclark687@gmail.com>
2024-06-01 21:43:25 -0400

    It's integration time baby

--- lib-stm32/build.rs
+++ lib-stm32/build.rs
@@ -1,9 +1,9 @@
 use std::fs;
 use std::io::{BufWriter, Write};
 
-fn generate_pitch_set(ref_freq: u16, octaves_below: i8, octaves_above: i8) {
+fn generate_pitch_set(ref_freq: u16, octaves_below: i8, octaves_above: i8) -> Vec<(String, u16)>{
     // (Ref pitch is assumed to be A4)
-    let mut pitches = Vec::<(&str, u16)>::new();
+    let mut pitches = Vec::<(String, u16)>::new();
     let scale : [&str; 12] = ["a", "as", "b", "c", "cs", "d", "ds", "e", "f", "fs", "g", "gs"];
     // Calculate ALL the pitches!
     for o in 1..octaves_below {
@@ -11,7 +11,7 @@ fn generate_pitch_set(ref_freq: u16, octaves_below: i8, octaves_above: i8) {
             // vector of (pitch name, freq in hertz)
             // -12 semitones for each octave + number of semitones above A in that octave
             let freq = generate_pitch_from_reference(ref_freq, (-12 * o) + semitone);
-            let pitch = (scale[semitone as usize], freq);
+            let pitch = (format!("{}{}", scale[semitone as usize], 4 - o), freq);
             pitches.push(pitch);
         }
     }
@@ -22,20 +22,23 @@ fn generate_pitch_set(ref_freq: u16, octaves_below: i8, octaves_above: i8) {
             // vector of (pitch name, freq in hertz)
             // +12 semitones for each octave + number of semitones above A in that octave
             let freq = generate_pitch_from_reference(ref_freq, (12 * o) + semitone);
-            let pitch = (scale[semitone as usize] + String::from(4 + o), freq);
+            let pitch = (format!("{}{}", scale[semitone as usize], 4 + o), freq);
             pitches.push(pitch);
         }
+    }
+    return pitches;
 }
 
-fn write_pitches_to_file(mut pitch_set: Vec<(&str, u16)>){
-    // Open the file (audio/pitches.rs)
-    let f = fs::File::create("./audio/pitches.rs").expect("Unable to create pitches.rs");
+fn write_pitches_to_file(pitch_set: Vec<(String, u16)>){
+    /// Write each pitch name as a constant
+    /// in the format PITCH_NAME_OCTAVE, ex. 
+    /// pub const A4: u16 = 440
+    let f = fs::File::create("./src/audio/pitches.rs").expect("Unable to create pitches.rs");
     let mut f = BufWriter::new(f);
-    // Write to it
     for pitch in pitch_set {
-        writeln!(f, "let {}: u16 = {}", pitch[0], pitch[1]);
+        let _ = writeln!(f, "pub const {}: u16 = {};", pitch.0.to_uppercase(), pitch.1);
     }
-    f.write_all(data.as_bytes()).expect("Unable to write pitch data");
+    let _ = f.flush();
 }
 
 fn generate_pitch_from_reference(ref_pitch: u16, semitones: i8) -> u16{
@@ -45,6 +48,6 @@ fn generate_pitch_from_reference(ref_pitch: u16, semitones: i8) -> u16{
 
 fn main() {
     let a4 = 440;
-    let mut pitch_set = generate_pitch_set(a4, 2, 2);
+    let pitch_set = generate_pitch_set(a4, 2, 2);
     write_pitches_to_file(pitch_set);
 }
