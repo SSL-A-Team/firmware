7bbf6f76a817996a9631f078adf64af4e6b2ec75
Christian Clark <christianclark687@gmail.com>
2024-07-03 23:24:25 -0400

    Better names

--- lib-stm32/src/math/linear.rs
+++ lib-stm32/src/math/linear.rs
@@ -1,27 +1,27 @@
 use num_traits::{FromPrimitive, ToPrimitive};
 use core::ops::{Add, Sub, Div, Mul};
 
-pub trait BoundNumeric: Copy + Sub<Output = Self> + Add<Output = Self> + 
+pub trait MappingNumeric: Copy + Sub<Output = Self> + Add<Output = Self> + 
     Div<Output = Self> + Mul<Output = Self> + FromPrimitive + ToPrimitive {}
 
 // Take input max and/or min (bounds), output value within new bounds
-struct Bounds<T>
+struct LinearMap<T>
 where
-    T: BoundNumeric 
+    T: MappingNumeric 
 {
     min: T,
     max: T,
 }
 
-impl<T> Bounds<T>
+impl<T> LinearMap<T>
 where
-    T: BoundNumeric 
+    T: MappingNumeric 
 {
     fn new(min: T, max: T) -> Self {
-        Bounds { min, max }
+        LinearMap { min, max }
     }
 
-    fn linear_map_to_new_bounds(&self, val: T, new_bounds: Bounds<T>) -> T {
+    fn linear_map_to_new_bounds(&self, val: T, new_bounds: LinearMap<T>) -> T {
         let min_diff = self.min + new_bounds.min;
         let scale = (new_bounds.max - new_bounds.min) / (self.max - self.min);
         let mapped_val = (val - min_diff) * scale;
