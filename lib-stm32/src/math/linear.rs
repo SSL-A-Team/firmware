1c2efe4207203ba5de13fbcf1358d42491aa39d3
Christian Clark <christianclark687@gmail.com>
2024-07-09 21:41:43 -0400

    Fix the clamping function to be good math

--- lib-stm32/src/math/linear.rs
+++ lib-stm32/src/math/linear.rs
@@ -24,9 +24,8 @@ where
     }
 
     pub fn linear_map_to_new_bounds(&self, val: T, new_bounds: LinearMap<T>) -> T {
-        let min_diff = self.min + new_bounds.min;
         let scale = (new_bounds.max - new_bounds.min) / (self.max - self.min);
-        let mapped_val = (val - min_diff) * scale;
+        let mapped_val = (val - self.min) * scale + new_bounds.min;
         return mapped_val;
     }
 }
