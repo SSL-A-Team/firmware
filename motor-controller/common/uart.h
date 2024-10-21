cab75cd2c4f0e4ec720057d4af4389d3eb0dee78
Joe Spall <jspall0327@gmail.com>
2024-10-20 22:19:19 -0700

    WIP continued review code

--- motor-controller/common/uart.h
+++ motor-controller/common/uart.h
@@ -1,15 +1,16 @@
 /**
  * @file uart.h
  * @author your name (you@domain.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-04-30
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #pragma once
+#define COMP_MODE
 
 ////////////////////////
 //  PUBLIC FUNCTIONS  //
@@ -24,3 +25,11 @@ bool uart_transmit(uint8_t *data_buf, uint16_t len);
 bool uart_can_read();
 uint8_t uart_read(void *dest, uint8_t len);
 void uart_discard();
+
+volatile uart_logging_status_t uart_logging_status;
+
+typedef enum {
+    UART_LOGGING_OK = 0,
+    UART_LOGGING_DMA_TX_ERROR,
+    UART_LOGGING_DMA_RX_ERROR
+} uart_logging_status_t;
