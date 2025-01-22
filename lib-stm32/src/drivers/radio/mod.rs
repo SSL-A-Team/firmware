4001c4512b9126510782c01d6675253e35caafab
Will Stuckey <guyfleeman@gmail.com>
2025-01-22 17:47:05 -0500

    all firmware moved to latest deps

--- lib-stm32/src/drivers/radio/mod.rs
+++ lib-stm32/src/drivers/radio/mod.rs
@@ -1,9 +1,3 @@
 pub mod at_protocol;
 pub mod edm_protocol;
 pub mod odin_w26x;
-
-#[derive(Debug)]
-pub enum RadioError {
-    SendCommandLowLevelBufferFull,
-    SendCommandInvalidCommunicationMode,
-}
