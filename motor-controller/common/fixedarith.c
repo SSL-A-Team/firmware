41e882d3f6964c60fbed5c9c6f5472f0ffe36fa6
Will Stuckey <guyfleeman@gmail.com>
2025-10-20 23:01:25 -0400

    initial fp math stufF

---
+++ motor-controller/common/fixedarith.c
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "fixedarith.h"
+
+typedef struct FixedPointConfig {
+    uint8_t total_bits;
+    uint8_t int_bits;
+    uint8_t frac_bits;
+} FixedPointConfig_t;
+
+typedef int16_t Int16FixedPoint_t;
+typedef int32_t Int16OperationContainer_t;
+typedef uint16_t Uint16FixedPoint_t;
+typedef uint32_t Uint16OperationContainer_t;
+typedef int32_t Int32FixedPoint_t;
+typedef int64_t Int32OperationContainer_t;
+typedef uint32_t Uint32FixedPoint_t;
+typedef uint64_t Uint32OperationContainer_t;
+
+const FixedPointConfig_t FixedU12F4Config = {
+    .total_bits = 16,
+    .int_bits = 12,
+    .frac_bits = 4,
+};
+
+const FixedPointConfig_t FixedS12F4Config = {
+    .total_bits = 16,
+    .int_bits = 12,
+    .frac_bits = 4,
+};
+
+/**
+ * Create an unsigned fixed point number with at least 16 bits of storage.
+ */
+Uint16FixedPoint_t uint16fixed_set_from_int(uint16_t val, const FixedPointConfig_t *new_config) {
+    return (val << new_config->frac_bits);
+}
+
+/**
+ * Create an unsigned fixed point number with at least 16 bits of storage from another unsigned fixed point number.
+ */
+Uint16FixedPoint_t uint16fixed_set_from_uint16fixed(Uint16FixedPoint_t val, const FixedPointConfig_t *val_config, const FixedPointConfig_t *new_config) {
+    if (val_config->frac_bits == new_config->frac_bits) {
+        return val;
+    } else if (val_config->frac_bits > new_config->frac_bits) {
+        return val >> (val_config->frac_bits - new_config->frac_bits);
+    } else {
+        return val << (new_config->frac_bits - val_config->frac_bits);
+    }
+}
+
+uint16_t uint16fixed_to_int(Uint16FixedPoint_t val, const FixedPointConfig_t *val_config) {
+    return val >> val_config->frac_bits;
+}
+
+/**
+ * Add two fixed point numbers, frac bits must be the same
+ */
+Uint16FixedPoint_t* uint16fixed_add(Uint16FixedPoint_t lhs, Uint16FixedPoint_t rhs) {
+    return lhs + rhs;
+}
+
+Uint16FixedPoint_t* uint16fixed_sub(Uint16FixedPoint_t lhs, Uint16FixedPoint_t rhs) {
+    return lhs - rhs;
+}
+
+Uint16FixedPoint_t* uint16fixed_mul(Uint16FixedPoint_t lhs, Uint16FixedPoint_t rhs, const FixedPointConfig_t *config) {
+    Uint16OperationContainer_t ires = (Uint16OperationContainer_t) lhs * (Uint16OperationContainer_t) rhs;
+
+    // (1 << config->frac_bits) term provides rounding
+    #ifdef FIXEDPOINT_ARITH_ROUND
+    return (Uint16FixedPoint_t) ((ires + (1 << config->frac_bits)) >> config->frac_bits);
+    #else
+    return (Uint16FixedPoint_t) (ires >> config->frac_bits);
+    #endif
+}
+
+Uint16FixedPoint_t* uint16fixed_div(Uint16FixedPoint_t numerator, Uint16FixedPoint_t denominator, const FixedPointConfig_t *config) {
+    Uint16OperationContainer_t tdiv = (Uint16OperationContainer_t) numerator * (Uint16OperationContainer_t) (1 << config->frac_bits);
+    return (Uint16FixedPoint_t) (tdiv / denominator);
+}
---
+++ motor-controller/common/fixedarith.h
@@ -0,0 +1,3 @@
+// uncomment to enable rounding numbers
+// NOTE: feature incomplete
+// #define FIXEDPOINT_ARITH_ROUND
