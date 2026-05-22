e022cf261225b77b947598f69c6dba14a8ff30e1
Nicholas Witten <nwitt12@gmail.com>
2026-05-21 21:37:08 -0700

    remove openloop hwtest

--- control-board/src/bin/hwtest-control/main.rs
+++ control-board/src/bin/hwtest-control/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use ateam_common_packets::{bindings::{BasicControl, BodyControlCommand, BodyControlMode, GlobalVelocityCommand, KickRequest}, radio::DataPacket};
+use ateam_common_packets::{bindings::{BasicControl, BodyControlCommand, BodyControlMode, GlobalVelocityCommand, KickRequest, LocalVelocityCommand}, radio::DataPacket};
 use embassy_executor::InterruptExecutor;
 use embassy_stm32::{interrupt, pac::Interrupt};
 use embassy_sync::pubsub::PubSubChannel;
@@ -189,22 +189,22 @@ async fn main(main_spawner: embassy_executor::Spawner) {
             _bitfield_1: Default::default(),
             _bitfield_align_1: Default::default(),
 
-            vision_position_update: [0.0, 0.0, 0.0],
+            vision_position_update: [0.0, 0.0, 0.0],  // No vision updates
 
-            body_control_mode: BodyControlMode::BCM_GLOBAL_VELOCITY,
+            body_control_mode: BodyControlMode::BCM_LOCAL_VELOCITY,
             kick_request: KickRequest::KR_ARM,
             play_song: 0,
             reserved2: [0; 1],
             
             kick_vel: 0.0,
-            dribbler_speed: -0.1,
+            dribbler_speed: -0.1,  // Spin the dribbler
 
-            cmd: BodyControlCommand { global_vel: GlobalVelocityCommand {
-                global_xd: 2.0,
-                global_yd: 0.0,
-                global_omega: 0.0,
-                max_linear_acc: 3.0,
-                max_angular_acc: 6.28,
+            cmd: BodyControlCommand { local_vel: LocalVelocityCommand {
+                local_xd: 1.0,  // Move forward at 1 m/s
+                local_yd: 0.0,
+                local_omega: 0.0,
+                max_linear_acc: 0.0,  // Use default limits
+                max_angular_acc: 0.0,  // Use default limits
             }},
         }));
     }
