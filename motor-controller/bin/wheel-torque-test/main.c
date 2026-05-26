f8b47a1b54b9accc793bc5b7c7fd90d1651165e9
Nicholas Witten <nwitt12@gmail.com>
2026-05-25 19:22:45 -0700

    remove stale iir filter inits in test wheel images

--- motor-controller/bin/wheel-torque-test/main.c
+++ motor-controller/bin/wheel-torque-test/main.c
@@ -143,7 +143,7 @@ int main() {
     quadenc_reset_encoder_delta();
 
     // setup the velocity filter
-    iir_filter_init(&encoder_filter, iir_filter_alpha_from_Tf(ENCODER_IIR_TF_MS, VELOCITY_LOOP_RATE_MS));
+    iir_filter_init(&encoder_filter, iir_filter_alpha_from_cutoff_hz(200.0f, VELOCITY_LOOP_RATE_MS));
 
     ////////////////////////////////////
     //  Current Sense Initialization  //
